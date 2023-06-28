b97e24a34aea3cc3b5e3cf81b8c69094232efc6d
techmoves <muhndlovu28@gmail.com>
2023-06-28 12:04:19 +0200

    Fix bug

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -2,16 +2,9 @@ import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { useSelector } from 'react-redux';
 import RocketData from './RocketData';
-// import { fetchRockets } from '../redux/rockets/rocketSlice';
 
 const Rockets = () => {
   const { rockets } = useSelector((store) => store.rockets);
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchRockets());
-  // }, [dispatch]);
-  // useDispatch,
-  // { useEffect }
 
   return (
     <div>
